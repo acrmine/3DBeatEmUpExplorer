6db89cf8af1a37c38daa3288da42e4da469c360d
Andrew Richmond <41588533+acrmine@users.noreply.github.com>
2025-11-04 19:12:22 -0800

    Fixed player movement and added ground collision detection

--- Prefabs/Player/player.gd
+++ Prefabs/Player/player.gd
@@ -1,59 +1,47 @@
 extends RigidBody3D
 
-var JUMP_VELOCITY := 20.0
+var JUMP_VELOCITY := 400.0
 
 var mouseSensitivity := 0.005
 var twistInput = 0.0
 var pitchInput = 0.0
-var playerPrevPos := self.global_position
-var playerCurrPos: Vector3
-var playerPosOffst: Vector3
 var jumpVector := Vector3.UP * JUMP_VELOCITY
+var touchingGround: bool = false
 
 @onready var playercamera := $"../TwistPivot/PitchPivot/Camera3D"
 @onready var twist_pivot := $"../TwistPivot"
 @onready var pitch_pivot := $"../TwistPivot/PitchPivot"
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	self.contact_monitor = true
+	self.max_contacts_reported = 20
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# Grab current position in this frame and calculate offset
-	playerCurrPos = self.global_position
-	playerPosOffst = playerCurrPos - playerPrevPos
-	
-	# Calculate inputs into 3D force vector
-	#var input := Vector3.ZERO
-	#input.x = Input.get_axis("move_left", "move_right")
-	#input.z = Input.get_axis("move_forward", "move_back")
+	touchingGround = false
+	for node in get_colliding_bodies():
+		if node.is_in_group("GroundObjects"):
+			touchingGround = true
+			break
+
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (twist_pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	# Apply calculated input forces
 	apply_central_force(direction *  1200.0 * delta)
 	
-	# Essentially parent these objects to the player without adding rotation
-	playercamera.global_position += playerPosOffst
-	twist_pivot.global_position += playerPosOffst
-	
-	if Input.is_action_just_pressed("jump"):
-		apply_central_force(jumpVector)
-	
 	# Apply camera rotation if any was added
 	twist_pivot.rotate_y(twistInput)
 	pitch_pivot.rotate_x(pitchInput)
 	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, 
-		deg_to_rad(-30), 
+		deg_to_rad(-60), 
 		deg_to_rad(30)
 	)
 	twistInput = 0.0
 	pitchInput = 0.0
-	
-	# Stow Curr position for next frames Prev position
-	playerPrevPos = playerCurrPos
 
 
 # Fired whenever input is given to the game (currently used for mouse)
@@ -68,5 +56,5 @@ func _input(event: InputEvent) -> void:
 			twistInput = -event.relative.x * mouseSensitivity
 			pitchInput = -event.relative.y * mouseSensitivity
 	
-	if event is InputEventKey and event.is_action_pressed("jump"):
+	if event is InputEventKey and event.is_action_pressed("jump") and touchingGround:
 		apply_central_force(jumpVector)
